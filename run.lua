abc3430f665c6c306443d3372d1423a0aed786ed
andy <andy@andydote.co.uk>
2015-04-28 19:58:39 +0100

    run: addPanel(), example usage

--- run.lua
+++ run.lua
@@ -1,5 +1,30 @@
 local addon, ns = ...
 
+local components = ns.components
+
 Dark.config = {
-	components = ns.components
+
+	components = components,
+
+	addPanel = function(self, name, buildChildren)
+
+		local panel = components:panel({ text = name })
+
+		local children = components:createDefaults({ parent = panel })
+
+		buildChildren(children)
+
+	end,
+
 }
+
+
+-- example:
+
+
+Dark.config:addPanel("Interrupt", function(panel)
+	panel:input(config, "enabled", "boolean")
+	panel:input(config, "channel", "text")
+	panel:input(config, "suffix", "text")
+	panel:input(config, "notify", "text")
+end)
