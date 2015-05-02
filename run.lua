46d026d6f54661d2a4fca47c1142c4d1898055dd
andy <andy@andydote.co.uk>
2015-05-02 15:28:03 +0100

    run: add test methods, example code

--- run.lua
+++ run.lua
@@ -1,21 +1,26 @@
 local addon, ns = ...
-
 local components = ns.components
 
 Dark.config = {
 
 	components = components,
 
+	createWindow = function(self)
+		self.window = components:window({ name = "DarkConfigui" })
+	end,
+
 	addPanel = function(self, name, buildChildren)
 
-		local panel = components:panel({ text = name })
+		local panel = components:panel({ parent = self.window })
 		local children = components:createDefaults({ parent = panel })
 
 		children.add = function(self, child)
 			panel:addChild(child)
-		end,
+		end
 
 		buildChildren(children)
+
+		self.window:addPanel(name, panel)
 
 	end,
 
@@ -23,11 +28,15 @@ Dark.config = {
 
 
 -- example:
+--[[
 
+local config = Dark.raids.config
 
 Dark.config:addPanel("Interrupt", function(panel)
-	panel:add(panel:input(config, "enabled", "boolean"))
-	panel:add(panel:input(config, "channel", "text"))
-	panel:add(panel:input(config, "suffix", "text"))
-	panel:add(panel:input(config, "notify", "text"))
+    panel:add(panel:input(config, "enabled", "boolean"))
+    panel:add(panel:input(config, "channel", "text"))
+    panel:add(panel:input(config, "suffix", "text"))
+    panel:add(panel:input(config, "notify", "text"))
 end)
+
+]]
